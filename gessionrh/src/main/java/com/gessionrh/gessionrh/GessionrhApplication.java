a869646fb39482b056ade4c215e4243b966c9de3
MidoNdb <ndbmido510@gmail.com>
2024-02-21 23:50:29 -1200

    on ajoute les projet spring  boot

---
+++ gessionrh/src/main/java/com/gessionrh/gessionrh/GessionrhApplication.java
@@ -0,0 +1,13 @@
+package com.gessionrh.gessionrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GessionrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GessionrhApplication.class, args);
+	}
+
+}
---
+++ gessionrh/src/test/java/com/gessionrh/gessionrh/GessionrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gessionrh.gessionrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GessionrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
